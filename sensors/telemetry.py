b7a0f5f2f9ab8a3fd51da93d027c86e8184bdbc3
potatofarmer <68509014+yamfarmerNA@users.noreply.github.com>
2026-03-17 15:51:58 -0500

    Improve docstring in telemetry_valid function
    Updated the docstring for clarity

--- sensors/telemetry.py
+++ sensors/telemetry.py
@@ -1,9 +1,13 @@
 # sensors/telemetry.py
-
+# Simulates telemetry verification from Arduino car. Real system will be replaced by wireless or serial communicatin
 import random
 
 def telemetry_valid():
     """
-    Simulates whether telemetry confirms correct target.
+    Simulate whether telemetry confirms the target.
+
+    Returns
+    ------
+        True if the drone should trust the vision tracking.
     """
     return random.choice([True, True, True, False])  # 75% valid
